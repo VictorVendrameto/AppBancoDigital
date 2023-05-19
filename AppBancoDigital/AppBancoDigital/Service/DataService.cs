7a5d86d0f54574af535b5ad731dedba7c8533192
VictorVendrameto <vendrametovictor@gmail.com>
2023-05-19 10:16:27 -0300

    Data Service

--- AppBancoDigital/AppBancoDigital/Service/DataService.cs
+++ AppBancoDigital/AppBancoDigital/Service/DataService.cs
@@ -1,10 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace AppBancoDigital.Service
 {
     public class DataService
     {
+        private static readonly string server = "https//10.0.2.2:8000";
+
+        protected static async Task<string> GetDataFromService(string rota) 
+        {
+            string json_response;
+
+            string uri = server + rota;
+
+            if (Connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                throw new Exception("Conecte-se à Internet.");
+            }
+            using (HttpClient client = new HttpClient()) 
+            {
+                HttpResponseMessage response = await client.GetAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    json_response = response.Content.ReadAsStringAsync().Result;
+                }
+                else
+                    throw new Exception(DecodeServerError(response.StatusCode));
+            }
+            return json_response;
+        }
+        protected static async Task<string> PostDataToService(string json_object, string rota) 
+        {
+            string json_response;
+
+            string uri = server + rota;
+
+            if (Connectivity.NetworkAccess != NetworkAccess.Internet) 
+            {
+                throw new Exception("Conecte-se à Internet.");
+            }
+            using (HttpClient client = new HttpClient()) 
+            {
+                HttpResponseMessage response = await client.PostAsync(uri,
+                    new StringContent(json_object, Encoding.UTF8, 
+                    "application/json"));
+
+                if(response.IsSuccessStatusCode) 
+                {
+                    json_response = response.Content.ReadAsStringAsync().Result;
+                }
+                else 
+                {
+                    throw new Exception(DecodeServerError(response.StatusCode));
+                }
+
+                return json_response;
+            }
+
+            private static string DecodeServerError(System.Net.HttpStatusCode status_code) 
+            {
+                string msg_error;
+
+                switch (status_code) 
+                {
+                    case System.Net.HttpStatusCode.BadRequest:
+                        msg_error = "A requisição não pode ser atendida agora. Tente mais tarde.";
+                        break;
+
+                    case System.Net.HttpStatusCode.NotFound:
+                        msg_error = "Recurso indisponível no momento. Tente mais tarde.";
+                        break;
+
+                    case System.Net.HttpStatusCode.InternalServerError:
+                        msg_error = "Nosso banco de dados está indisponível. Tente mais tarde.";
+                        break;
+
+                    case System.Net.HttpStatusCode.RequestTimeout:
+                        msg_error = "O servidor está demorando muito para responder. Tente novamente.";
+                        break;
+
+                    case System.Net.HttpStatusCode.Forbidden:
+                        msg_error = "Recurso temporariamente indisponível. Tente mais tarde.";
+                        break;
+
+                    default:
+                        msg_error = "Estamos com dificuldades para acessar nosso servidor, tente novamente.";
+                        break;
+                }
+                return msg_error;
+            }
+        }
     }
 }
---
+++ AppBancoDigital/AppBancoDigital/Service/DataServiceCorrentista.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using AppBancoDigital.Model;
+using System.Threading.Tasks;
+
+namespace AppBancoDigital.Service
+{
+    public class DataServiceCorrentista : DataService
+    {
+        public static async Task<Correntista> AuthenticCorrentista(Correntista c)
+        {
+            var json_to_send = JsonConvert.SerializeObject(c);
+            string json = await DataService.PostDataToService(json_to_send, "/correntista/entrar");
+
+            Correntista correntista = new Correntista();
+            if (json != "false") 
+            {
+                correntista = JsonConvert.DeserializeObject<Correntista>(json);
+            }
+            return correntista;
+        }
+
+        public static async Task <Correntista> SignOn (Correntista c)
+        {
+            var json_to_send = JsonConvert.SerializeObject(c);
+
+            string json = await DataService.PostDataToService(json_to_send, "/correntista/save");
+
+            Console.WriteLine("_____________________________________________");
+            Console.WriteLine(json);
+            Console.WriteLine("_____________________________________________");
+
+            Correntista correntista = JsonConvert.DeserializeObject<Correntista>(json);
+
+            return correntista;
+        }
+    }
+}
